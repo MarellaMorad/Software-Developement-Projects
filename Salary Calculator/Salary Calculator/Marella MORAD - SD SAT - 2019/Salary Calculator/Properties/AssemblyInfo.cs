12725db324799056a5ec00e647b6c08dfce894e1
Marella Morad <marella99mourad@gmail.com>
2022-05-01 15:48:27 +1000

    Rename

--- Salary Calculator/Salary Calculator/Marella MORAD - SD SAT - 2019/Salary Calculator/Properties/AssemblyInfo.cs
+++ Salary Calculator/Salary Calculator/Marella MORAD - SD SAT - 2019/Salary Calculator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Marella MORAD - SD SAT - 2019")]
+[assembly: AssemblyTitle("Salary Calculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Marella MORAD - SD SAT - 2019")]
+[assembly: AssemblyProduct("Salary Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
